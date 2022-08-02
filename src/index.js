b519c679b1b2803689fe8eb1473ecdc865dd53e9
Jonathan Cheng <jcheng461@gmail.com>
2022-08-02 21:56:29 +0900

    add buttons to user interface for each soil and water function

--- src/index.js
+++ src/index.js
@@ -14,10 +14,33 @@ import {
 
 // User Interface Logic
 $(document).ready(function () {
+  // Soil Buttons
   $("#feed").click(function () {
+    const newState = stateControl(feed);
+    $("#soil-value").text(`Soil: ${newState.soil}`);
+  });
+  $("#blue-food").click(function () {
     const newState = stateControl(blueFood);
     $("#soil-value").text(`Soil: ${newState.soil}`);
   });
+  $("#green-food").click(function () {
+    const newState = stateControl(greenFood);
+    $("#soil-value").text(`Soil: ${newState.soil}`);
+  });
+  $("#yucky-food").click(function () {
+    const newState = stateControl(yuckyFood);
+    $("#soil-value").text(`Soil: ${newState.soil}`);
+  });
+  // Water Buttons
+  $("#hydrate").click(function () {
+    const newState = stateControl(hydrate);
+    $("#water-value").text(`Water: ${newState.water}`);
+  });
+  $("#super-water").click(function () {
+    const newState = stateControl(superWater);
+    $("#water-value").text(`Water: ${newState.water}`);
+  });
+  // General Buttons
   $("#show-state").click(function () {
     const currentState = stateControl();
     $("#soil-value").text(`Soil: ${currentState.soil}`);
